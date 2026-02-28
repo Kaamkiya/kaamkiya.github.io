77b935a51e3444e0ac17389dfb7fc0029ea3cdc4
Kaamkiya <ziam3.1415@gmail.com>
2026-02-27 23:13:20 -0500

    feat: Greatly improve mobile navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://kaamkiya.github.io",
-  integrations: [icon({
+  // TODO: add icons for external links (maybe into a footer?)
+  /*integrations: [icon({
     "simple-icons": ["github", "matrix", "mastodon"],
-  })],
+  })],*/
 });
