1a930f03f126f5afc40ce45ad5f99741b672a9b5
Kaamkiya <ziam3.1415@gmail.com>
2026-07-22 10:55:56 -0400

    feat: Add RSS feed (/feeds/rss.xml)

---
+++ src/pages/feeds/rss.xml.js
@@ -0,0 +1,18 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import sanitizeHtml from "sanitize-html";
+
+export async function GET(context) {
+  const postImportResult = import.meta.glob('../posts/**/*.md', { eager: true });
+  const posts = Object.values(postImportResult);
+
+  return rss({
+    title: "Kaamkiya's Blog",
+    description: "The ramblings of a random programmer",
+    site: context.site,
+    items: await Promise.all(posts.map(async (post) => ({
+      link: post.url,
+      content: sanitizeHtml((await post.compiledContent())),
+      ...post.frontmatter,
+    }))),
+  });
+}
